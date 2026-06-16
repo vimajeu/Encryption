25b6ff0f964e748419d8d38a6385d909a698427f
vimajeu <vitalina.hatsura.25@kse.org.ua>
2026-06-16 12:26:59 +0300

    extern/export

--- cipher_api.h
+++ cipher_api.h
@@ -5,17 +5,23 @@
 #ifndef ENCRYPTION_CIPHER_API_H
 #define ENCRYPTION_CIPHER_API_H
 
+#ifdef _WIN32
+ #define EXPORT __declspec(dllexport)
+#else
+ #define EXPORT __attribute__((visibility("default")))
+#endif
+
 typedef void* cipher_t;
 
-/* Factory — creates the cipher object and returns an opaque handle */
-cipher_t* cipher_create_caesar (int key);
-cipher_t* cipher_create_vigenere(const char* key);
-/* Operations — behave differently depending on which cipher was
-created */
-char* cipher_encrypt(cipher_t* cipher, const char* text);
-char* cipher_decrypt(cipher_t* cipher, const char* text);
-/* Cleanup */
-void cipher_destroy(cipher_t* cipher);
-void cipher_free (char* str);
+extern "C" {
+      EXPORT cipher_t* cipher_create_caesar (int key);
+      EXPORT cipher_t* cipher_create_vigenere(const char* key);
+
+      EXPORT char* cipher_encrypt(cipher_t* cipher, const char* text);
+      EXPORT char* cipher_decrypt(cipher_t* cipher, const char* text);
+
+      EXPORT void cipher_destroy(cipher_t* cipher);
+      EXPORT void cipher_free (char* str);
+}
 
 #endif //ENCRYPTION_CIPHER_API_H
